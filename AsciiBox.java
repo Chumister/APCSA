b16df8bd5761b60064d1b8855d12fed719bdf755
League Student <league@adminisorsiMac3.attlocal.net>
2020-02-02 11:04:08 -0800

    fgad

--- AsciiBox.java
+++ AsciiBox.java
@@ -8,8 +8,9 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
-		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("Alan Chu \t  December 13, 2002 \n Computer Science Principles \n" );
+		System.out.println("Test Project" );
 
 	}
+	
 }
---
+++ Variables.java
@@ -0,0 +1,73 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+		
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = -32123;
+		int intOne = 90877;
+		long longOne = 999999999;
+		float floatone = 546;
+		double doubleOne = 345;
+		char charOne = 34;
+		boolean booleanone = true;
+		String stringone = "hey";
+		
+
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longone = "+longOne+"\t\t*");
+		System.out.println("*32 bit - floatOne = "+floatone+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*booleanOne = "+booleanone+"\t\t*");
+		System.out.println("*stringone = "+stringone+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
