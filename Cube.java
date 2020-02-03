c3b024eea3c9926784aa33d7965647b8cbbb5a2d
Chumister <alanchu4432@yahoo.com>
2020-02-03 14:38:37 -0800

    cvdfsd

---
+++ Cube.java
@@ -0,0 +1,16 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		int volume;
+		int volume2;
+		volume = side*side;
+		volume2= side*6;
+		return side;
+	}
+}
