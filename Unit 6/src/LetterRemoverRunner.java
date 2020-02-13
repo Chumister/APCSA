797b607532eff7377be4875ae1c9790375afdd7a
Chumister <alanchu4432@yahoo.com>
2020-02-13 14:06:08 -0800

    sfdfd

---
+++ Unit 6/src/LetterRemoverRunner.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class LetterRemoverRunner
+{
+	public static void main( String args[] )
+	{
+		LetterRemover test = new LetterRemover
+	   (test.setRemover("I really want dumplings","l"));
+											
+}
