269e26a8dda0a2b9f81ce0ec02246119e427a634
Chumister <alanchu4432@yahoo.com>
2020-02-27 13:35:39 -0800

    sg

--- Unit 8/src/DownRunner.java
+++ Unit 8/src/DownRunner.java
@@ -7,16 +7,11 @@ public class DownRunner
 {
 	public static void main( String args[] )
 	{
-<<<<<<< HEAD:Unit 8/src/DownRunner.java
 		System.out.println(RayDown.go(new int [] {-99,1,2,3,4,5,6,7,8,9,10,12345}));
-=======
-		System.out.println(RayDown.go(new int [] {-99,1,2,3,4,5,6,7,8,9,10,5}));
->>>>>>> c0b56c240086c6cfc0f28e48c279f792e981570b:Unit 8/DownRunner.java
 		System.out.println(RayDown.go(new int [] {10,9,8,7,6,5,4,3,2,1,-99}));
 		System.out.println(RayDown.go(new int [] {10,20,30,40,50,-11818,40,30,20,10}));
 		System.out.println(RayDown.go(new int [] {32767}));
 		System.out.println(RayDown.go(new int [] {255,255}));
-<<<<<<< HEAD:Unit 8/src/DownRunner.java
 		System.out.println(RayDown.go(new int [] {9,10,-88,100,-555,1000}));
 		System.out.println(RayDown.go(new int [] {9,8,7,6,5,4,3,2,0,-2,-989}));
 		System.out.println(RayDown.go(new int [] {12,15,18,21,23,1000}));
@@ -25,13 +20,3 @@ public class DownRunner
 
 	}
 }
-=======
-		System.out.println(RayDown.go(new int [] {9,10,-88,100,-555,2}));
-		System.out.println(RayDown.go(new int [] {9,8,7,6,5,4,3,2,0,-2,6}));
-		System.out.println(RayDown.go(new int [] {12,15,18,21,23,1000}));
-		System.out.println(RayDown.go(new int [] {250,19,17,15,13,11,10,9,6,3,2,1,0}));
-		System.out.println(RayDown.go(new int [] {9,10,-8,-5000,-3000}));
-
-	}
-}
->>>>>>> c0b56c240086c6cfc0f28e48c279f792e981570b:Unit 8/DownRunner.java
