88ed0eb0929012f079af86a11dcd5a343aa8d3d1
Chumister <alanchu4432@yahoo.com>
2020-03-02 14:29:28 -0800

    cvdgsf

--- Unit 9/src/DeckTester.java
+++ Unit 9/src/DeckTester.java
@@ -18,7 +18,7 @@ public class DeckTester {
 		    System.out.println();
 		    System.out.println("How large is the deck?");
 		    System.out.println(cards.size());
-		    cards.shuffle();
+		    cards.shuffle(values);
 		    cards.deal();
 		    System.out.println();
 		    System.out.println(cards.toString());
