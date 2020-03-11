4f084e8e1aaa92a4547f34d348b90948d4abd047
Chumister <alanchu4432@yahoo.com>
2020-03-11 14:40:37 -0700

    drhtrgzsj

--- Unit9ArrayLists/src/ListDown.java
+++ Unit9ArrayLists/src/ListDown.java
@@ -10,10 +10,10 @@ public class ListDown
 {
 	//go() will return true if all numbers in numArray
 	//are in decreasing order [31,12,6,2,1]
-	public static boolean go(int[] numArray)
+	public static boolean go(List<Integer>numArray)
 	{
-		for (int num = 0; (num < numArray.length && num+1< numArray.length); num ++)
-			if (numArray[num] > numArray[num+1])
+		for (int num = 0; (num < numArray.size() && num+1< numArray.size()); num ++)
+			if (numArray.get(num) > numArray.get(num+1))
 				return true;
 			else 
 				return false;
