9eb4cd0de4d39b6f543708d19c0e7db7b27f1207
Chumister <alanchu4432@yahoo.com>
2020-03-17 19:52:21 -0700

    ajh

--- Unit11/src/ToyStoreRunner.java
+++ Unit11/src/ToyStoreRunner.java
@@ -11,6 +11,12 @@ public class ToyStoreRunner
 {
 	public static void main( String args[] )
 	{
-
+		ArrayList toys=new ArrayList<Toy>();
+		Toy h=new Toy(1,"sorry");
+		System.out.println(h);
+		Toy k=new Toy(4,"gi joe");
+		System.out.println(k);
+		toys.add(h);
+		toys.add(k);
 	}
 }
