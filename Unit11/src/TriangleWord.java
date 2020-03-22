652d5a9f89ced47516d1e87f0cc5e7185a439720
Chumister <alanchu4432@yahoo.com>
2020-03-22 15:31:31 -0700

    affdg

--- Unit11/src/TriangleWord.java
+++ Unit11/src/TriangleWord.java
@@ -9,7 +9,14 @@ public class TriangleWord
 {
 	//instance variables and constructors could be present, but are not necessary
 		
-	public static void printTriangle(String word)
-	{
+	public static void printTriangle(String word) {
+		int a = 1;
+		for (int i = 0; i < word.length(); i++) {
+			for (int j = 0; j < a; j++) {
+				ApcsProject.p(word.substring(0, a));
+			}
+			ApcsProject.pL();
+			a++;
+		}
 	}
 }
